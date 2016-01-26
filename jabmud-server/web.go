3782d5f53bb853694d2f4a469148d3f41b84f48a
Tony Rasa <trasa@meancat.com>
2016-01-26 15:23:02 -0800

    base Bootstrap hello world index works

--- jabmud-server/web.go
+++ jabmud-server/web.go
@@ -2,8 +2,9 @@ package main
 import (
 	"log"
 	"net/http"
-	"html/template"
 	"fmt"
+	"io"
+	"time"
 )
 
 const webBaseDir = "web"
@@ -20,24 +21,16 @@ func render(w http.ResponseWriter, r *http.Request) {
 	requestedFile := r.URL.Path[1:]
 	log.Printf("render: %s", requestedFile)
 
-	templ := fmt.Sprintf("%s/%s", webBaseDir, requestedFile)
-	t, err := template.ParseFiles(templ)
-	if err != nil {
-		log.Printf("template parsing error: %s - %s", requestedFile, err)
-		http.NotFound(w, r)
+	f, err := http.Dir(webBaseDir).Open(requestedFile)
+	defer f.Close()
+
+	if err == nil {
+		content := io.ReadSeeker(f)
+		http.ServeContent(w, r, requestedFile, time.Now(), content)
 		return
 	}
-	err = t.Execute(w, "") // TODO data ?
-	if err != nil {
-		log.Printf("template execution error: %s - %s", requestedFile, err)
-		// some sort of 500 error
-	}
-
-
-/*
-	// if name 'console.html' doesn't match template filename then stuff doesn't work.
-	t := template.New("console.html")
-	t, _ = t.ParseFiles("web/console.html")
-	t.Execute(w, "")
-	*/
+	log.Printf("Error opening requested file %s: %v", requestedFile, err)
+	// TODO return some sort of 500 error,
+	// and return 404 only for when the file 'merely' can't be found..
+	http.NotFound(w, r)
 }

b9652ebe8c4f39c896b306405c19ff58ee402712
Tony Rasa <trasa@meancat.com>
2016-01-26 11:32:41 -0800

    opens webserver on 8888
    overly-simple edition

--- jabmud-server/main.go
+++ jabmud-server/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"github.com/emgee/go-xmpp/src/xmpp"
 	"log"
+	"net/http"
+//	"github.com/braintree/manners"
+	"fmt"
 )
 
 type Command struct {
@@ -10,6 +13,21 @@ type Command struct {
 }
 
 func main() {
+	go connectHttpServer()
+	connectComponent()
+}
+
+func connectHttpServer() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, this is %s", r.URL.Path[1:])
+	})
+	http.ListenAndServe(":8888", nil)
+}
+
+
+
+func connectComponent() {
+	// connect as component
 	jid, _ := xmpp.ParseJID("jabmud.localhost")
 	stream, _ := xmpp.NewStream("localhost:5275", nil)
 	X, _ := xmpp.NewComponentXMPP(stream, jid, "secret")
@@ -25,11 +43,11 @@ func main() {
 			X.Out <- xmpp.Message{Body: "hi!", To: v.From, From: v.To}
 		case *xmpp.Iq:
 			log.Printf("iq: ", v.Payload)
-			/* doesn't work
-			foo := Command {}
-			v.PayloadDecode(foo)
-			log.Printf("decoded: %v", foo)
-			*/
+		/* doesn't work
+		foo := Command {}
+		v.PayloadDecode(foo)
+		log.Printf("decoded: %v", foo)
+		*/
 		default:
 			log.Printf("%T: %v\n", v, v)
 		}

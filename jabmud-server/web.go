4a36bf8ef79806d4aff1e4e34c4766d59ac6cbc8
Tony Rasa <trasa@meancat.com>
2016-01-26 14:59:33 -0800

    static console and index files served by looking at requested url

--- jabmud-server/web.go
+++ jabmud-server/web.go
@@ -3,24 +3,41 @@ import (
 	"log"
 	"net/http"
 	"html/template"
+	"fmt"
 )
 
+const webBaseDir = "web"
+const port = 8888
 
 func ConnectHttpServer() {
-	http.HandleFunc("/favicon.ico", faviconHandler)
-	http.HandleFunc("/", rootHandler)
-	log.Println("http listening on port 8888")
-	http.ListenAndServe(":8888", nil)
+	http.HandleFunc("/", render)
+	log.Printf("http listening on port %d", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
 
-func faviconHandler(w http.ResponseWriter, r *http.Request) {
-	// nothing happens
-}
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("rootHandler: %s", r.URL.Path[1:])
+func render(w http.ResponseWriter, r *http.Request) {
+	requestedFile := r.URL.Path[1:]
+	log.Printf("render: %s", requestedFile)
+
+	templ := fmt.Sprintf("%s/%s", webBaseDir, requestedFile)
+	t, err := template.ParseFiles(templ)
+	if err != nil {
+		log.Printf("template parsing error: %s - %s", requestedFile, err)
+		http.NotFound(w, r)
+		return
+	}
+	err = t.Execute(w, "") // TODO data ?
+	if err != nil {
+		log.Printf("template execution error: %s - %s", requestedFile, err)
+		// some sort of 500 error
+	}
+
+
+/*
 	// if name 'console.html' doesn't match template filename then stuff doesn't work.
 	t := template.New("console.html")
 	t, _ = t.ParseFiles("web/console.html")
 	t.Execute(w, "")
+	*/
 }

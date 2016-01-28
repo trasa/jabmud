c9260323907dffc56ea5049d08763825a3f7a5ba
Tony Rasa <trasa@meancat.com>
2016-01-27 22:53:41 -0800

    basic impl of github.com/julienschmidt/httprouter
    the Hello World of routing.

--- jabmud-server/web.go
+++ jabmud-server/web.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/julienschmidt/httprouter"
 	"io"
 	"log"
 	"net/http"
@@ -12,10 +13,22 @@ const webBaseDir = "web"
 const port = 8888
 const defaultPage = "index.html"
 
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+}
+
 func connectHttpServer() {
-	http.HandleFunc("/", render)
+
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/hello/:name", Hello)
+
 	log.Printf("http listening on port %d", port)
-	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), router)
 }
 
 func render(w http.ResponseWriter, r *http.Request) {

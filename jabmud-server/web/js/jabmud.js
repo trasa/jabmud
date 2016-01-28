4c4d45fcdad8ae0e8fea3dff2beaa83b816f0c0b
Tony Rasa <trasa@meancat.com>
2016-01-27 22:22:39 -0800

    console UI is active now

--- jabmud-server/web/js/jabmud.js
+++ jabmud-server/web/js/jabmud.js
@@ -6,6 +6,36 @@
             console.log("this.get #/");
             this.render('templates/index.ejs', function(html) {
                 $('#mainContent').html(html);
+
+                function displayMessage(msg) {
+                    var display = $('#display');
+                    display
+                        .append(msg + "\n\n")
+                        .stop()
+                        .animate({ scrollTop: display[0].scrollHeight}, 800);
+                }
+
+                function send() {
+                    var buf = $('#buf');
+                    var msg = buf.val();
+                    console.log("sending '" + msg + "'");
+                    displayMessage("[sending '" + msg + "']");
+                    buf.val('');
+                }
+
+                $('#send').click(function(e) {
+                    send();
+                });
+
+                $('#buf')
+                    .bind("enterKey", function(e) {
+                        send();
+                    })
+                    .keyup(function (e) {
+                        if (e.keyCode == 13) {
+                            $(this).trigger('enterKey');
+                        }
+                    });
             });
         });
     });

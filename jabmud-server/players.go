3aaad522f5e848ccaaa7d7943f81cfca73af27e0
Tony Rasa <trasa@meancat.com>
2016-02-19 14:09:34 -0800

    Player Stringer

--- jabmud-server/players.go
+++ jabmud-server/players.go
@@ -10,7 +10,10 @@ type Player struct {
 	Name string
 	Jid  string
 }
-// go:generate stringer -type=Player
+
+func (p Player) String() string {
+	return fmt.Sprintf("(Player Id='%s', Name='%s', Jid='%s'", p.Id, p.Name, p.Jid)
+}
 
 var knownPlayersById = make(map[string]Player)
 var knownPlayersByJid = make(map[string]Player)
@@ -23,7 +26,7 @@ func Login(player Player) error {
 	if player.Id == "" {
 		return fmt.Errorf("Login: JID '%s' didn't provide a valid Id", player.Jid)
 	}
-	log.Printf("Player '%s' (%s) (%s) logging in", player.Name, player.Id, player.Jid)
+	log.Printf("%s logging in", player)
 	knownPlayersById[player.Id] = player
 	knownPlayersByJid[player.Jid] = player
 	return nil
@@ -33,7 +36,7 @@ func Logout(player Player) error {
 	if player.Id == "" {
 		return fmt.Errorf("Logout: JID '%s' didn't provide a valid Id", player.Jid)
 	}
-	log.Printf("Player '%s' (%s) (%s) logged out", player.Name, player.Id, player.Jid)
+	log.Printf("%s logged out", player)
 	delete(knownPlayersById, player.Id)
 	delete(knownPlayersByJid, player.Jid)
 	return nil
